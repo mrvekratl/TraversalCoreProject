5b3800667feafbc7389aa538a301822dd9b93120
mervekiratli55 <mervekiratli.mk@gmail.com>
2025-05-13 20:14:31 +0300

    updates

--- BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a97727e1e072988c5f9233217b62f3ce2ccef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24819cd5eae07993877524925dfcf28b4dbc309c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TraversalCoreProject/obj/Debug/net8.0/TraversalCoreProject.AssemblyInfo.cs
+++ TraversalCoreProject/obj/Debug/net8.0/TraversalCoreProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TraversalCoreProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a97727e1e072988c5f9233217b62f3ce2ccef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24819cd5eae07993877524925dfcf28b4dbc309c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TraversalCoreProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TraversalCoreProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
