fb63bf4ada13f2b261da4602d2ec04dd32509a22
mervekiratli55 <mervekiratli.mk@gmail.com>
2025-06-02 18:49:09 +0300

    ASP.NET Core Camp: End of day 6

--- EntityLayer/obj/Debug/net8.0/EntityLayer.AssemblyInfo.cs
+++ EntityLayer/obj/Debug/net8.0/EntityLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+558c8780fcccb749eb435f424b518ce2315aed75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7052614a6cbee2c58d9c2c691de92723b9e4e39")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
